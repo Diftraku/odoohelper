e3c7351722c1522edb3d88ffaf9c6d4d055bd6a7
Diftraku <Diftraku@users.noreply.github.com>
2025-05-19 10:40:51 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='odoohelper',
-    version='0.1.0',
-    description='ODOO CLI to help common tasks',
+    name="odoohelper",
+    version="0.1.1",
+    description="ODOO CLI to help common tasks",
     long_description=readme,
-    author='Ville Valtokari',
-    author_email='ville.valtokari@ecxol.net',
-    url='https://github.com/denvil/odoohelper',
+    author="Ville Valtokari",
+    author_email="ville.valtokari@ecxol.net",
+    url="https://github.com/denvil/odoohelper",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=("tests", "docs")),
     install_requires=[
-        'Click',
-        'openerp_proxy',
-        'colorama',
-        'keyring',
-        'pytz',
-        'holidays',
-        'textile'
+        "Click",
+        "openerp_proxy",
+        "colorama",
+        "keyring",
+        "pytz",
+        "holidays",
+        "textile",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         odoohelper=odoohelper.odoohelper:main
-    '''
+    """,
 )
